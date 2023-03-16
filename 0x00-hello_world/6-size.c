3c7fe650bf3c2ab98182fea44b54a5974dc6fd85
victortking <fredrickrichard39@gmail.com>
2023-03-16 12:36:03 -0700

    alx-low_level_programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,15 +12,15 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %c byte(s)\n", sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 
-	printf("Size of an int: %i byte(s)\n", sizeof(i));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
 
-	printf("Size of a long long int: %lld byte(s)\n", sizeof(lli));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
 
-	printf("Size of a float: %f byte(s)\n", sizeof(f));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
