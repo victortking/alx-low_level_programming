6c2b732826989b03a8e90f359f2109856862a8cb
victortking <fredrickrichard39@gmail.com>
2023-03-16 08:11:40 -0700

    alx-low_level_programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  main - initializing main function
+ *  Return: returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
