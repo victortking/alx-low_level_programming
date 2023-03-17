fce3bfa5dffd7651eef1193bac33b98a28e5be15
victortking <fredrickrichard39@gmail.com>
2023-03-17 15:13:50 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
