63401a22a628ba869d954ad2c496dc75afcab01e
victortking <fredrickrichard39@gmail.com>
2023-03-22 06:03:09 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('o');
+	_putchar(r + '0');
+
+	r = _islower(108);
+	_putchar(r + '0');
+
+	r = _islower('c');
+	_putchar(r + '0');
+
+	r = _islower('I');
+	_putchar(r + '0');
+
+	r = _islower('s');
+	_putchar(r + '0');
+
+	r = _islower(70);
+	_putchar(r + '0');
+
+	r = _islower(20);
+	_putchar(r + '0');
+
+	r = _islower('U');
+	_putchar(r + '0');
+
+	r = _islower('n');
+	_putchar(r + '0');
+
+	r = _islower(0);
+	_putchar(r + '0');
+
+	r = _islower('H');
+	_putchar(r + '0');
+
+	_putchar('\n');
+
+	return (0);
+}
