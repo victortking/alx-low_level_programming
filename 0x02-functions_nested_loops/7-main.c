d19acdf448853978c350a7d326560c492c2c75d5
victortking <fredrickrichard39@gmail.com>
2023-03-22 14:40:07 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <limits.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(98);
+	_putchar(r + '0');
+
+	r = print_last_digit(0);
+	_putchar(r + '0');
+
+	r = print_last_digit(-1024);
+	_putchar(r + '0');
+
+	r = print_last_digit(INT_MAX);
+	_putchar(r + '0');
+
+	r = print_last_digit(INT_MIN);
+	_putchar(r + '0');
+
+	r = print_last_digit(1);
+	_putchar(r + '0');
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - entry point
+ * @n: parameter to check
+ * Return: returns last digit
+ */
+int print_last_digit(int n)
+{
+	int lastn;
+
+	lastn = n % 10;
+
+	return (lastn);
+}
