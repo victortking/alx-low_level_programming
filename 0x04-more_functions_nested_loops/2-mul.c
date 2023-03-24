ea1c303e0acf0fc2083b01e88c37bb564bf1541e
victortking <fredrickrichard39@gmail.com>
2023-03-24 00:33:51 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ * @a: parameter to multiply
+ * @b: parameter to multiply
+ * Return: returns m
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+
+	return (m);
+}
