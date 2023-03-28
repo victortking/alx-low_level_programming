36f2a0045a3540f7081fc824284f9af0a5b9af31
victortking <fredrickrichard39@gmail.com>
2023-03-28 14:50:33 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int num = 0;
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + (s[i] - '0');
+
+		if (num > 0 && (s[i] < '0' || s[i] > '9'))
+			break;
+
+		i++;
+	}
+
+	return (num * sign);
+}
