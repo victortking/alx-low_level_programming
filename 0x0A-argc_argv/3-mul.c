72f3a6eb028dee05ab15602b6a020aaddd692b2b
victortking <fredrickrichard39@gmail.com>
2023-04-07 07:29:30 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: rerurns error if unsuccessful otherwise 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 0;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("ERROR\n");
+		return 1;
+	}
+	
+	for (i = 0; i < atoi(argv[1]); i++)
+			mul += atoi(argv[2]);
+
+
+	printf("%d\n", mul);
+	return (0);
+
+}
