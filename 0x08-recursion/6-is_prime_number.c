7afc135ae37afb94bf19935d6c5eeffc1ec6ff2d
victortking <fredrickrichard39@gmail.com>
2023-04-06 19:19:33 -0700

    0x08-recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,16 +9,16 @@
  */
 int is_prime_helper(int n, int i)
 {
-    if (n <= 1)
-        return (0);
+	if (n <= 1)
+		return (0);
 
-    if (i >= n)
-        return (1);
+	if (i >= n)
+		return (1);
 
-    if (n % i == 0)
-        return (0);
+	if (n % i == 0)
+		return (0);
 
-    return (is_prime_helper(n, i + 1));
+	return (is_prime_helper(n, i + 1));
 }
 
 /**
@@ -29,7 +29,7 @@ int is_prime_helper(int n, int i)
  */
 int is_prime_number(int n)
 {
-    return (is_prime_helper(n, 2));
+	return (is_prime_helper(n, 2));
 }
 
 
