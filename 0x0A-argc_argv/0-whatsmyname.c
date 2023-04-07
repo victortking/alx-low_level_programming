3a5f7ebec96727e0743329e80d87dc9d14c89a43
victortking <fredrickrichard39@gmail.com>
2023-04-07 05:05:07 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns integer value
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (argc);
+}
