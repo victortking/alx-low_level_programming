4f656c8d833d174a0bbdf14103a6fc28207488f5
victortking <fredrickrichard39@gmail.com>
2023-04-06 20:04:38 -0700

    0x08-recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,18 +9,18 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '\0') /* '*' is the last character, matches anything */
-            return (1);
-        if (*s1 == '\0') /* the first string is empty but '*' matches an empty string */
-            return (wildcmp(s1, s2 + 1));
-        return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)); /* try to match with or without current character */
-    }
-    if (*s1 == '\0') /* the first string is empty but we reached the end of the second string */
-        return (*s2 == '\0');
-    if (*s1 != *s2) /* current characters do not match */
-        return (0);
-    return (wildcmp(s1 + 1, s2 + 1)); /* recursively compare the rest of the strings */
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0') /* '*' is the last character, matches anything */
+			return (1);
+		if (*s1 == '\0') /* the first string is empty but '*' matches an empty string */
+			return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)); /* try to match with or without current character */
+	}
+	if (*s1 == '\0') /* the first string is empty but we reached the end of the second string */
+		return (*s2 == '\0');
+	if (*s1 != *s2) /* current characters do not match */
+		return (0);
+	return (wildcmp(s1 + 1, s2 + 1)); /* recursively compare the rest of the strings */
 }
 
