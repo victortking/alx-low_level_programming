c007e93edb2f4cbf8baa0a40040410647c39fa73
victortking <fredrickrichard39@gmail.com>
2023-04-06 19:49:52 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,14 +9,16 @@
  */
 int is_palindrome(char *s)
 {
+    size_t len = strlen(s);
+
     /* base case: empty or one-character string is always a palindrome */
-    if (*s == '\0' || *(s + 1) == '\0')
+    if (len == 0 || len == 1)
         return 1;
 
     /* recursive case: check if first and last characters match,
      * and recursively check the remaining substring */
-    if (*s == *(s + strlen(s) - 1))
-        return is_palindrome(s + 1) && is_palindrome(s + strlen(s) - 1);
+    if (*s == *(s + len - 1))
+        return is_palindrome(s + 1) && is_palindrome(s + len - 2);
     else
         return 0;
 }
