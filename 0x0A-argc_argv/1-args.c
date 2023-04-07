6994624fcb281f2e3966bd1a23407de26682493e
victortking <fredrickrichard39@gmail.com>
2023-04-07 05:57:35 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: returns 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
