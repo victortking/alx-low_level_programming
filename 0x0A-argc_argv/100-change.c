958ad3c6c7d894099049730daed345c638069481
victortking <fredrickrichard39@gmail.com>
2023-04-09 11:11:05 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents = atoi(argv[1]), coins = (0);
+	int cents = atoi(argv[1]), coins = 0;
 
 	if (argc != 2)
 	{
@@ -21,34 +21,16 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	while (cents > 0)
-	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else
-		{
-			cents -= 1;
-			coins++;
-		}
-	}
+	coins += cents / 25;
+	cents %= 25;
+	coins += cents / 10;
+	cents %= 10;
+	coins += cents / 5;
+	cents %= 5;
+	coins += cents / 2;
+	cents %= 2;
+	coins += cents;
+	cents %= 1;
 	printf("%d\n", coins);
 	return (0);
 }
