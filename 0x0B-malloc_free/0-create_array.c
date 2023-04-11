4316135099c46cf78ac2b382791d2261e2314623
victortking <fredrickrichard39@gmail.com>
2023-04-11 03:45:38 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_array - allocates memory
+ * @size: size of array
+ * @c: value of array
+ * Return: returns char value
+ */
+char *create_array(unsigned int size, char c)
+{
+	char*m;
+
+	if (size == 0)
+		return (NULL);
+
+	m = malloc(sizeof(char));
+
+	if (m == NULL)
+		return NULL;
+	else
+		m[0] = c;
+
+	return (m);
+}
