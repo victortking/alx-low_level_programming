e2f78beb81a6d7e9b97e97647173b4b4263d8a70
victortking <fredrickrichard39@gmail.com>
2023-04-11 03:53:03 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	m = malloc(sizeof(char) * 1);
+	m = malloc(sizeof(char) * 4);
 
 	if (m == NULL)
 		return NULL;
