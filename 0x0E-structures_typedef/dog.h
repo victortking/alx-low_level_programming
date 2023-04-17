a19b37d15341e5bfe1f808fa6c07e6a84b016eaa
victortking <fredrickrichard39@gmail.com>
2023-04-17 04:24:59 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - stores members for a dog profile
+ * @name: dogs's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 struct dog
 {
 	char *name;
