f17ffc766c6761eb10ed0b3495554162ede5be69
victortking <fredrickrichard39@gmail.com>
2023-04-30 04:20:29 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,7 +14,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node, *last_node;
 
 	if (head == NULL)
-        return (NULL);
+		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
