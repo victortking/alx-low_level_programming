c616d1385faea5bffa7ea878a40cdc42affaf8f8
victortking <fredrickrichard12@gmail.com>
2023-06-07 08:38:24 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,24 +2,22 @@
 #include <string.h>
 
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: the string to check
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: The string to check
  *
  * Return: 1 if s is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-    size_t len = strlen(s);
+	int len = strlen(s);
 
-    /* base case: empty or one-character string is always a palindrome */
-    if (len == 0 || len == 1)
-        return 1;
+	if (len <= 1)
+		return (1);
+	
+	if (s[0] != s[len - 1])
+			return (0);
 
-    /* recursive case: check if first and last characters match,
-     * and recursively check the remaining substring */
-    if (*s == *(s + len - 1))
-        return is_palindrome(s + 1) && is_palindrome(s + len - 2);
-    else
-        return 0;
+	s[len - 1] = '\0';
+
+	return is_palindrome(s + 1);
 }
-
