2564c29c063f4cadf464d1ce7e9549ef7bca30d7
victortking <fredrickrichard12@gmail.com>
2023-06-07 09:28:54 -0700

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,38 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
 /**
- * is_palindrome - Checks if a string is a palindrome
- * @s: The string to check
+ * is_palindrome_recursive - Recursive helper function to check if a string is a palindrome.
+ * @s: The input string.
+ * @start: The starting index of the current substring.
+ * @end: The ending index of the current substring.
  *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * Return: 1 if the substring is a palindrome, 0 otherwise.
+ */
+int is_palindrome_recursive(char *s, int start, int end)
+{
+    if (start >= end)
+        return 1;
+
+    if (!isalnum(s[start]))
+        return is_palindrome_recursive(s, start + 1, end);
+
+    if (!isalnum(s[end]))
+        return is_palindrome_recursive(s, start, end - 1);
+
+    if (tolower(s[start]) != tolower(s[end]))
+        return 0;
+
+    return is_palindrome_recursive(s, start + 1, end - 1);
+}
+
+/**
+ * is_palindrome - Check if a string is a palindrome.
+ * @s: The input string.
+ *
+ * Return: 1 if @s is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
     int len = strlen(s);
-    int i, j;
-
-    if (len <= 1)
-        return 1;
-
-    i = 0;
-    j = len - 1;
-
-    while (i < j)
-    {
-        while (!isalnum(s[i]) && i < j)
-            i++;
-        while (!isalnum(s[j]) && i < j)
-            j--;
-
-        if (tolower(s[i]) != tolower(s[j]))
-            return 0;
-
-        i++;
-        j--;
-    }
-
-    return 1;
+    return is_palindrome_recursive(s, 0, len - 1);
 }
-
