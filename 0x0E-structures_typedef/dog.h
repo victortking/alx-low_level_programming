2078f8854ceae6723f2740504f4a82ee32e37bb7
victortking <fredrickrichard12@gmail.com>
2023-06-19 12:53:35 -0700

    quick fix

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - stores members for a dog profile
+ * @name: dogs's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: dog's info and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
