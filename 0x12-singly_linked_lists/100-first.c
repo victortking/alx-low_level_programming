441333b426c8e9db23884f87f4e46dc5e4313efb
victortking <fredrickrichard12@gmail.com>
2023-07-03 04:24:05 -0700

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * before_main - Constructor function executed before main
+ *
+ * Description: This function is executed automatically before the main
+ * function is called. It prints two lines of text using the puts function.
+ */
+void __attribute__((constructor)) before_main()
+{
+    const char *line1 = "You're beat! and yet, you must allow,";
+    const char *line2 = "I bore my house upon my back!";
+
+    puts(line1);
+    puts(line2);
+}
