95fd9b278c579d7aeddc45a43b479271d5a5471e
victortking <fredrickrichard12@gmail.com>
2023-07-03 02:47:16 -0700

    sing

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a node
+ * @h: head of nod3
+ * Return: returns count of the node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+
+		count++;
+
+		h = h->next;
+	}
+
+	return (count);
+}
